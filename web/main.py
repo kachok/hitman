d09bec5ff2f2489bd4a00b3cf738ef79cabf0a11
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2012-01-05 06:10:04 -0500

    missed changes

--- web/main.py
+++ web/main.py
@@ -1,6 +1,11 @@
-from bottle import route, run, view, debug, static_file, request, response
+from bottle import route, run, view, debug, static_file, request, response, abort
 
 import settings
+from languages import langs
+
+import json
+import psycopg2
+
 
 # get IP address, including handling of proxies
 # based on http://stackoverflow.com/questions/4581789/how-do-i-get-user-ip-address-in-django
@@ -16,10 +21,14 @@ def get_client_ip(request):
 @route('/static/:filename')
 def server_static(filename):
     return static_file(filename, root='./static/')
-    
+
+@route('/static/images/:filename')
+def server_static(filename):
+    return static_file(filename, root='./static/images')
+   
 @route('/')
 def index():
-    return '<b>Hello World! reload %s</b>' % (get_client_ip(request))
+    return '<b>Hello World!'
 
 # simple JSON webservice to return client IP address
 @route('/ip')
@@ -27,16 +36,82 @@ def index():
     response.content_type = 'application/json'
     return {"ip":get_client_ip(request)}
 
+# simple JSON webservice to return words for specific HITId
+@route('/words')
+def index():
+	
+	hitid=request.query.hitId
+	
+	try:
+		conn = psycopg2.connect("dbname='hitman' user='dkachaev' host='localhost'")
+	except:
+		pass
+		
+	cur = conn.cursor()
+	
+	sql="select * from vocabularyHITs vh, vocabulary v where vh.hit_id=%s and v.id=vh.word_id order by random()"
+	sql="select * from vocabularyhitsdata d, vocabulary v, vocabularyhits h where h.id=d.hit_id and v.id=d.word_id and hitid=%s"
+	
+	print hitid
+	cur.execute(sql, (hitid,))
+	
+	rows=cur.fetchall()
+	
+	words=[]
+	for row in rows:
+		words.append({"word_id":row[2],"word":row[5]})
+	
+	conn.close()
 
-@route('/main')
-@view('main')
-def main(name='World reload'):
-    params={
-        "ipinfodb_key":settings.settings["ipinfodb_key"],
-		"hit_type":"vocabulary-ru"
-    }
-    return dict(params=params)
+
+	response.content_type = 'application/json'
+	return {"words":words}
+
+@route('/notifications')
+def notifications():
+
+	return None
+
+
+
+@route('/hits/vocabulary/<language>')
+@view('vocabulary')
+def vocabulary_hit(language):
+    #when page is rendered, get assignmentID/hitID and attach it to displayed results
+
+	assignmentid=request.query.assignmentId
+	hitid=request.query.hitId
+	
+	params={
+		"ipinfodb_key":settings.settings["ipinfodb_key"],
+		"hit_type":"vocabulary-ru",
+		"assignmentid":assignmentid,
+		"hitid":hitid,
+		"lang":language,
+		"lang_name":langs[language],
+		"ip":get_client_ip(request),
+		#"words":json.dumps(words),
+		}
+	return dict(params=params)
+
+@route('/hits/synonyms')
+@view('synonyms')
+def synonyms_hit():
+    #when page is rendered, get assignmentID/hitID and attach it to displayed results
+
+	assignmentid=request.query.assignmentId
+	hitid=request.query.hitid
+
+	params={
+		"ipinfodb_key":settings.settings["ipinfodb_key"],
+		"hit_type":"vocabulary-ru",
+		"assignmentid":assignmentid,
+		"hitid":hitid,
+		"ip":get_client_ip(request),
+		#"words":json.dumps(words),
+		}
+	return dict(params=params)
 
 debug(True)
-run(reloader=True, port=8880)
+run(reloader=True, port=8888)
 #run(host='localhost', port=8800)
