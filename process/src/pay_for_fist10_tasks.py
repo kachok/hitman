cca390174584cd2e04083e9a3fcd7f52f9a94827
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2012-02-16 09:28:14 -0500

    pay first 10 tasks script

---
+++ process/src/pay_for_fist10_tasks.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+import mturk
+from settings import settings
+import psycopg2
+
+# basic logging setup for console output
+import logging
+logging.basicConfig(
+	format='%(asctime)s %(levelname)s %(message)s', 
+	datefmt='%m/%d/%Y %I:%M:%S %p',
+	level=logging.INFO)
+
+logging.info("pay for first 10 tasks - START")
+
+target_language = settings["target_language"]
+logging.info("target language: %s" % (target_language))
+
+try:
+	conn = psycopg2.connect("dbname='"+settings["dbname"]+"' user='"+settings["user"]+"' host='"+settings["host"]+"'")
+	logging.info("successfully connected to database")
+except:
+	logging.error("unable to connect to the database")
+
+cur = conn.cursor()
+cur2 = conn.cursor()
+cur3 = conn.cursor()
+	
+# select worker_id total_assignments done worker_id assignments paid
+sql="select * from (select worker_id, count(*) from assignments a group by worker_id) as total left join (select worker_id, count(*) from assignments a where a.mturk_status in ('Approved', 'Rejected') group by worker_id) as paid on total.worker_id=paid.worker_id;"
+cur.execute(sql)
+rows = cur.fetchall()
+
+mturk_conn=mturk.conn()
+
+total_paid=0
+
+for row in rows:
+	worker_id=str(row[0])
+	done=str(row[1])
+	paid=str(row[3])
+
+	#print "worker: ", worker_id, " tasks done: ",done, ", tasks paid: ", paid
+	
+	#if worker was never paid
+	if paid=='None':
+		paid=0
+
+	#if worker was paid less than 10 times, get his unpaid assignments
+	paid=int(paid)
+	if paid<10:
+		to_pay=10-paid
+		total_paid=total_paid+1
+		print "worker: ", worker_id, " tasks done: ",done, ", tasks paid: ", paid, ", paying for ", to_pay," tasks right now (if available)"
+
+		sql2="select * from assignments where worker_id=%s and mturk_status='Submitted' limit %s;"
+		
+		cur2.execute(sql2, (worker_id,to_pay,))
+		rows2 = cur2.fetchall()
+	
+		for row2 in rows2:
+			assignment_id=str(row2[0])
+			mturk_assignment_id=str(row2[1])
+			hit_id=str(row2[2])
+			print "hit_id: ",hit_id
+			
+			#TODO: replace feedback with something generic
+			try:
+				pass
+				#mturk_conn.approve_assignment(mturk_assignment_id, feedback=settings["synonyms_approve_feedback"])
+			except:
+				print "failed to update status in MTurk"
+			
+			
+			#sql3="UPDATE assignments SET mturk_status='Approved' WHERE id=%s;"
+			#cur3.execute(sql3, (assignment_id,))
+			#conn.commit()
+
+			#sql3="UPDATE hits SET approved=approved+1 WHERE id=%s;"
+			#cur3.execute(sql3, (hit_id,))
+			#conn.commit()
+
+conn.close()
+	
+print "total workers paid: ",total_paid
+logging.info("FINISH")
