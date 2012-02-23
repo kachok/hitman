a6f602e763a2da820cce70a59b0c40ac7254e519
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2012-02-23 02:23:46 -0500

    fix for obama4america script, for rare languages

--- backend/src/obama4australia.py
+++ backend/src/obama4australia.py
@@ -7,7 +7,10 @@ import codecs
 words=["Barack Obama", "Australia"]
 
 langs="ru,ur,de,fr,eu,eo,sl,fi,ca,sv,lt,war,nl,da,sk,he,pl,cs,hr,hu,it,bg,sr,kk,uk,ro,ja,ms,pt,ko,vi,tr,es,fa,id,ar,zh,hi,io,an,mk,mg,pms,ka,bs,wa,ga,cv,diq,ht,nds,nap,sq,hy,sh,tt,qu,yo,ceb,th,ne,ku,am,te,tl,su,ml,mr,jv,ta,gu,sw,no,kn,pnb".split(",")
+langs="bn,bh,or,bpy,pa,bo,ks,pnb,gu,te,hi,ta,kn,ml,mr,ms,ks,sd,ps,uz,tk,af,am,so,rw,wo,ilo,pam,bcl,pag"
+langs=langs.split(',')
 
+print langs
 lang="en"
 
 dict={}
@@ -19,6 +22,8 @@ for word in words:
 	links= wikipydia.query_language_links(title=word, language=lang, limit=1000)
 	#print len(links)
 	for link in links:
+		#print ">>", link
+		
 		if link in dict:
 			dict[link][word]=(links[link])
 		else:
@@ -37,9 +42,10 @@ for l in langs:
 				text=text+word+" : "
 				
 		
-		print text
+		#print text
 	else:
-		print "---"		
+		#print "---"
+		pass		
 		
 print "prep str2img files"
 
@@ -50,15 +56,20 @@ fs=open("support_sequence.txt","w")
 for l in langs:
 	text=""
 	for i, word in enumerate(words):
-		#print i, len(words)
-		if word in dict[l]:
-			text=text+dict[l][word]
-			if i!=len(words)-1: 
-				text=text+" and "
+		if l in dict:
+			if word in dict[l]:
+				text=text+dict[l][word]
+				if i!=len(words)-1: 
+					text=text+" and "
+			else:
+				text=text+word
+				if i!=len(words)-1: 
+					text=text+" and "
 		else:
 			text=text+word
 			if i!=len(words)-1: 
 				text=text+" and "
+			
 			
 	fw.write(text+"\n")
 	fs.write(l+"\n")
