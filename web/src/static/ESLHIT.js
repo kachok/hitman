6b4e413537ba0684c487afae0e963809a945490b
Ellie Pavlick <epavlick@jhu.edu>
2012-07-10 13:39:05 -0400

    html fixes

--- web/src/static/ESLHIT.js
+++ web/src/static/ESLHIT.js
@@ -163,7 +163,7 @@ function getHelp() {
 */
 function writeOriginalSentences() {
 	for ( var s = 0; s < sentences.length; s++) {
-		$("#sent" + s).text(sentences[s]);
+		$("#sent" + s).html(sentences[s]);
 	}
 }
 
