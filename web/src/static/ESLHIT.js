e173748dd518a072f6cf3a5769391d53ee1cf8f2
Ellie Pavlick <epavlick@jhu.edu>
2012-07-10 18:30:07 -0400

    fixed problem loading sentences

--- web/src/static/ESLHIT.js
+++ web/src/static/ESLHIT.js
@@ -162,16 +162,16 @@ function getHelp() {
 * Write text of all original sentences to display as reference for user
 */
 function writeOriginalSentences() {
-/*	for ( var s = 0; s < sentences.length; s++) {
+	for ( var s = 0; s < sentences.length; s++) {
 		$("#sent" + s).text(sentences[s]);
-	}*/
-	for(var i = 0; i < words.length; i++){
+	}
+	/*for(var i = 0; i < words.length; i++){
 		var s = "";
 		for(var j = 0; i < words[i].length; j++){
-			s += word[i][j]+ " ";
+			s += words[i][j]+ " ";
 		}
 		$("#sent" + 1).text(s);
-	}
+	}*/
 }
 
 /**
