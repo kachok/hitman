fedc11384e9d8f0b54061f312ed468e1cc9db070
Ellie Pavlick <epavlick@jhu.edu>
2012-08-13 17:02:38 -0400

    more error generation fixes

--- esl/src/cntrl/gen-errors.py
+++ esl/src/cntrl/gen-errors.py
@@ -7,9 +7,9 @@ def deterr(words, idx):
 	pos = nltk.pos_tag(words)
 	if(idx > 0 and pos[idx - 1][1] == 'DT'):
 		if(random.getrandbits(1)):
-			return changedet(words, idx)
+			return changedet(words, idx - 1)
 		else:
-			words[idx] = ""
+			words[idx - 1] = '[]'
 			return words
 	else:
 		return adddet(words, idx)
@@ -54,6 +54,13 @@ def spellerr(words, idx):
 	return words
 
 def verberr(words, idx):
+	beverbs = ['be', 'is', 'am', 'are', 'was', 'were', 'being']
+	if words[idx] in beverbs:
+		return beverberr(words, idx)
+	else:
+		return otherverberr(words, idx)
+
+def otherverberr(words, idx):
 	endings = {'ing' : r'(.*)ing\Z', 'ed' : r'(.*)ed\Z', 'es' : r'(.*)es\Z', 's' : r'(.*)s\Z'}
 	for end in endings:
 		m = re.match(endings[end], words[idx])
@@ -65,6 +72,15 @@ def verberr(words, idx):
 			return words
 	nend = endings.keys()[random.randint(0, len(endings.keys()) - 1)]
 	words[idx] = '['+words[idx] + nend+']'
+	return words
+
+def beverberr(words, idx):
+	beverbs = ['be', 'is', 'am', 'are', 'was', 'were', 'being']	
+	w = words[idx]
+	nbe = beverbs[random.randint(0, len(beverbs) - 1)]
+	while(w == nbe):
+		nbe = beverbs[random.randint(0, len(beverbs) - 1)]
+	words[idx] = '['+nbe+']'
 	return words
 
 def getpos(sent):
@@ -84,21 +100,47 @@ def getpos(sent):
 	return {"verb" : verb, "prep" : prep, "noun" : noun}
 
 def randerr(sent):
+	alteredidx = []
 	words = sent.split()
 	if(len(words) > 0):
 		pos = getpos(sent)
 		verb = pos['verb']
 		prep = pos['prep']
 		noun = pos['noun']
-		sidx = random.randint(0, len(words) - 1)
-		ws = spellerr(words, sidx)
-		words = ws
+		#introduce preposition errors
+		timeout = 0
 		if(len(prep) > 0):
 			pidx = random.randint(0, len(prep) -1)
+			while(timeout < 50 and pidx in alteredidx):
+				timeout += 1
+				pidx = random.randint(0, len(prep) -1)
 			words = preperr(words, prep[pidx])
+			alteredidx.append(pidx)
+		#introduce determiner errors
+		timeout = 0
+		if(len(noun) > 0):
+			nidx = random.randint(0, len(noun) -1)
+			while(timeout < 50 and nidx in alteredidx):
+				timeout += 1
+				nidx = random.randint(0, len(noun) -1)
+			words = deterr(words, noun[nidx])
+			alteredidx.append(nidx)
+		#introduce verb errors
+		timeout = 0
 		if(len(verb) > 0):
 			vidx = random.randint(0, len(verb) -1)
+			while(timeout < 50 and vidx in alteredidx):
+				timeout += 1
+				vidx = random.randint(0, len(verb) -1)
 			words = verberr(words, verb[vidx])
+		#introduce spelling errors
+		timeout = 0
+		sidx = random.randint(0, len(words) - 1)
+		while(timeout < 50 and sidx in alteredidx):
+				timeout += 1
+				sidx = random.randint(0, len(words) -1)
+		words = spellerr(words, sidx)
+	
 	s = ""
 	for w in words:
 		s += w + " "
